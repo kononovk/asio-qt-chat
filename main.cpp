89aab55f8798de06048564f15682cb46359f12bc
kononovk <kononov.nikolay.nk1@gmail.com>
2020-12-20 15:37:18 +0300

    text typing added

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    QTextEdit textEdit;
+    QPushButton sendButton("send");
+
+    QObject::connect(&sendButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+    QVBoxLayout layout;
+    layout.addWidget(&textEdit);
+    layout.addWidget(&sendButton);
+
+    QWidget window;
+    /* 17 */ window.setLayout(&layout);
+    window.show();
+
+
+    return QApplication::exec();
 }
